a4f588cfc64000bf9af230022d71448a0ce982b2
Pilar Marinas <pilar.marinas@upcnet.es>
2014-01-15 18:03:24 +0100

    Afegir google maps amb adreca indicada equipament

--- vilaix/theme/browser/content_views/equipament.py
+++ vilaix/theme/browser/content_views/equipament.py
@@ -26,3 +26,28 @@ class View(grok.View):
         url_imatge = '%s/++widget++form.widgets.image/@@download/%s' % (url.replace("view", "@@edit"), self.context.image.filename)
        
         return url_imatge
+    
+    def getMapa(self):
+        catalog = getToolByName(self.context, 'portal_catalog')
+        utool = getToolByName(self.context, 'portal_url')
+       
+        caracter = "+"
+        adreca_cont = ''
+        cp = ''
+        poblacio = ''
+
+        if self.context.adreca_contacte:
+            adreca_cont = self.context.adreca_contacte.split()
+        
+        if self.context.codi_postal:
+            cp = self.context.codi_postal.split()
+        
+        if self.context.poblacio:
+            poblacio = self.context.poblacio.split()
+        
+        adreca = caracter.join(adreca_cont) + '+' + caracter.join(cp) + '+' + caracter.join(poblacio)
+
+        mapa = '<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.es/maps?f=q&amp;source=s_q&amp;hl=ca&amp;geocode=&amp;q=%s;aq=&amp;ie=UTF8&amp;hq=&amp;hnear=%s;t=m&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe>' % (adreca, adreca)
+        return mapa
+
+
