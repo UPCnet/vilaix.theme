37972848eb9d80893491ea70ef847fc71c5b62e7
Roberto Diaz <roberto.diaz@upcnet.es>
2014-01-20 13:29:14 +0100

    solucionar breadcrumb en explotacion

--- vilaix/theme/navigation.py
+++ vilaix/theme/navigation.py
@@ -22,8 +22,10 @@ class PhysicalNavigationBreadcrumbs(BrowserView):
     def breadcrumbs(self):
         real_context = aq_inner(self.context)
         request_path = self.request.get('PATH_INFO', '')
+        
         if 'VirtualHostBase' in request_path:
-            path = ('',) + re.search(r'VirtualHostBase/\w+/.*?/(.*?)/VirtualHostRoot/(?:.*?/)?(.*)', request_path).groups()
+            path = ('',) + re.search(r'VirtualHostBase/\w+/.*?/(.*?)/VirtualHostRoot/(.*)', request_path).groups()
+            #path = ('',) + re.search(r'VirtualHostBase/\w+/.*?/(.*?)/VirtualHostRoot/(?:.*?/)?(.*)', request_path).groups()
             request_path = '/'.join(path)
         context_path = '/'.join(real_context.getPhysicalPath())
 
