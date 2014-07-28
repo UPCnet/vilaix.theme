0274bdb576e066c991727db1e93532a9bd11a76c
Pilar Marinas <pilar.marinas@upcnet.es>
2014-07-28 09:30:01 +0200

    Modificar tramit perque es puguin afegir fitxers addicionals

--- vilaix/theme/browser/content_views/tramit.py
+++ vilaix/theme/browser/content_views/tramit.py
@@ -20,25 +20,37 @@ class View(grok.View):
     # def getImage(self):
     #     catalog = getToolByName(self.context, 'portal_catalog')
     #     utool = getToolByName(self.context, 'portal_url')
-        
+
     #     url = self.request.getURL()
 
-    #     if self.context.image:           
+    #     if self.context.image:
     #         url_imatge = '%s/++widget++form.widgets.image/@@download/%s' % (url.replace("view", "@@edit"), self.context.image.filename)
     #     else:
     #         url_imatge = ''
-      
+
     #     return url_imatge
-    
+
     def getFitxer(self):
         catalog = getToolByName(self.context, 'portal_catalog')
         utool = getToolByName(self.context, 'portal_url')
 
         url = self.request.getURL()
-        
-        if self.context.fitxer_inici:           
+
+        if self.context.fitxer_inici:
             url_fitxer = '%s/++widget++form.widgets.fitxer_inici/@@download/%s' % (url.replace("view", "@@edit"), self.context.fitxer_inici.filename)
         else:
             url_fitxer = ''
-      
+
         return url_fitxer
+
+    def fitxers_addicionals(self):
+        fitxers = self.context.contentItems()
+        url = self.request.getURL()
+        results = []
+        for fitxer in fitxers:
+            url_fitxer = '%s/@@download/file/%s' % (url.replace("view", fitxer[0]), fitxer[1].file.filename)
+            dades = {'filename': fitxer[1].file.filename,
+                     'url_fitxer': url_fitxer}
+            results.append(dades)
+
+        return results
