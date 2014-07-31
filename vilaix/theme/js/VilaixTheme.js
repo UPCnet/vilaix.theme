e09220a1f8cb7a0a0e9d2a7397f0cd560d785897
Pilar Marinas <pilar.marinas@upcnet.es>
2014-07-31 09:48:47 +0200

    Modificar menu principal que al passar per sobre es deplegui el menu i al clicar es vagi a la carpeta

--- vilaix/theme/js/VilaixTheme.js
+++ vilaix/theme/js/VilaixTheme.js
@@ -1,1 +1,11 @@
 // Main custom JS
+
+$(document).ready(function () {
+
+    $('#portal-globalnav ul.nav li.dropdown').hover(function() {
+      $(this).find('.dropdown-menu').stop(true, true).delay(0).fadeIn();
+    }, function() {
+      $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut();
+    });
+
+});
