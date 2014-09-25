8eec808d01ba796dd495a3b331c829a39fc9f976
Pilar Marinas <pilar.marinas@upcnet.es>
2014-09-25 17:33:10 +0200

    Borrar codigo comentado

--- vilaix/theme/browser/navigationPeu.py
+++ vilaix/theme/browser/navigationPeu.py
@@ -6,6 +6,7 @@ from Products.CMFCore.utils import getToolByName
 
 from plone.app.layout.viewlets.common import GlobalSectionsViewlet
 
+
 class navigationPeu(GlobalSectionsViewlet):
     index = ViewPageTemplateFile('viewlets_templates/navigationPeu.pt')
 
@@ -28,22 +29,20 @@ class navigationPeu(GlobalSectionsViewlet):
 
         results = []
         for fold in folders:
-            #if cont <= 4:
-                if fold.exclude_from_nav == False:
-                    results.append(dict(id_menu_titulo=fold.Title,
-                                        id_menu_url=fold.getURL(),
-                                        id_menu='m' + str(cont),
-                                        sub_menus = [i for i in portal_catalog.searchResults(path = dict(query=fold.getPath(), depth=1),
-                                                                                  review_state=['internally_published','external','published'],
-                                                                                  sort_on='getObjPositionInParent') if i.exclude_from_nav == False],
-                                        id_sub_menus = 'sm' + str(cont),
-                                        selected = self.context.absolute_url().startswith(fold.getURL()) and 'selected' or None,
-                                        tab_is_root = tabs_root
-                                        )
-                                   )
-                    cont = cont + 1
-                else:
-                    continue
-            #else:
-                #continue
+            if fold.exclude_from_nav == False:
+                results.append(dict(id_menu_titulo=fold.Title,
+                                    id_menu_url=fold.getURL(),
+                                    id_menu='m' + str(cont),
+                                    sub_menus = [i for i in portal_catalog.searchResults(path = dict(query=fold.getPath(), depth=1),
+                                                                              review_state=['internally_published','external','published'],
+                                                                              sort_on='getObjPositionInParent') if i.exclude_from_nav == False],
+                                    id_sub_menus = 'sm' + str(cont),
+                                    selected = self.context.absolute_url().startswith(fold.getURL()) and 'selected' or None,
+                                    tab_is_root = tabs_root
+                                    )
+                               )
+                cont = cont + 1
+            else:
+                continue
+
         return results
